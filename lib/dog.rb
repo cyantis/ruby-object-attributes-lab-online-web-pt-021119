87ed80f6a5c53b5be7d5fca799bea3aebc2cf0a5
Cody Yantis <cyantis@gmail.com>
2019-02-27 01:38:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 class Dog
-  def initialize(name)
-    @name = name
+  def name=(dog_name)
+   this_dogs_name = dog_name
+ end
 
-  def name
-    @name
-  end
+ def name
+   this_dogs_name
+ end
 end
